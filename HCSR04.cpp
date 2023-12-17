ba1bd209601bda4ddbf48c2a9ddaebb852fe168b
Aditya Agarwal <aditya.agarwal030504@gmail.com>
2023-12-17 10:52:46 +0530

    feat: add library for non-blocking operation
    Signed-off-by: Aditya Agarwal <aditya.agarwal030504@gmail.com>

---
+++ HCSR04.cpp
@@ -0,0 +1,267 @@
+#include "HCSR04.h"
+
+/** Maximum Distance the sensor should be able to measure before readings are considered invalid/too far awat */
+constexpr auto MAX_DISTANCE = 300;
+/** Timeout of the sensor based on the maximum distance it can measure */
+constexpr auto SENSOR_TIMEOUT = MAX_DISTANCE * 20'000ms / 343;
+
+// Constructors
+
+HCSR04::HCSR04(PinName trig, PinName echo)
+        : trigPin(trig)
+        , echoPin(echo)
+        , pulseBusyLock(0, 1)
+        , shouldTerminate(1, 1)
+        , threadRunLock(0, 1)
+{
+
+    echoPin.rise(callback(this, &HCSR04::pulse_start_handler));
+    echoPin.fall(callback(this, &HCSR04::pulse_end_handler));
+}
+
+// Public Methods
+
+bool
+HCSR04::initialize() {
+
+    // return if the thread was already initialized; move forward otherwise
+    // allocate the thread and return if the allocation fails; move forward otherwise
+    // try to start the thread and return if successful
+    // in case of failure, delete the allocated thread and return
+
+    if (is_initialized()) {
+        return false;
+    }
+
+    threadHandle = new (std::nothrow) Thread(osPriorityRealtime);
+    if (threadHandle == nullptr) {
+        return false;
+    }
+
+    auto status = threadHandle->start(callback(this, &HCSR04::dispatch_events));
+    if (status != osOK) {
+
+        delete threadHandle;
+        threadHandle = nullptr;
+
+        return false;
+    }
+
+    return true;
+}
+
+bool
+HCSR04::finalize() {
+
+    // return if the thread was already initialized, there are pending non-periodic measurements or a periodic measurement; move forward otherwise
+    // allocate the thread and return if the allocation fails; move forward otherwise
+    // try to start the thread and return if successful
+    // in case of failure, delete the allocated thread and return
+
+    if (!is_initialized() || is_periodic_started() || get_pending_measurement_count() > 0) {
+        return false;
+    }
+
+    shouldTerminate.acquire();
+    queue.break_dispatch();
+    threadRunLock.acquire();
+
+    auto status = threadHandle->terminate();
+    if (status != osOK) {
+        return false;
+    }
+    shouldTerminate.release();
+
+    delete threadHandle;
+    threadHandle = nullptr;
+    return true;
+}
+
+bool
+HCSR04::is_initialized() const {
+
+    // if threadHandle is NULL, then the object was not initialized/finalized before
+    // otherwise it is still in the initialized state
+
+    return threadHandle != nullptr;
+}
+
+bool
+HCSR04::do_measurement(const Callback<void(bool, float)> &cb) {
+
+    // return if a periodic event is already registered; move forward otherwise
+    // otherwise, post a non-periodic event to the queue, where the distance is measured and the callback called
+    // increment the pending measurement count if the event was successfully posted
+
+    if (is_periodic_started()) {
+        return false;
+    }
+
+    auto id = queue.call([this, cb]() {
+
+        // start a pulse and sleep on the lock while the pulse does not return
+        // the lock is released in HCSR04::pulse_end_handler() when the pulse is completely received
+        // if the pulse takes too long (faulty sensor or object too far away), then wake-up anyway
+
+        start_pulse();
+
+        pulseBusyLock.try_acquire_for(SENSOR_TIMEOUT)
+        ? cb(true, dist)
+        : cb(false, 0.0f);
+
+        dec_pending_measurements();
+    });
+
+    if (id == 0) {
+        return false;
+    }
+
+    inc_pending_measurements();
+    return true;
+}
+
+uint32_t
+HCSR04::get_pending_measurement_count() const {
+
+    return pendingMeasurementCount;
+}
+
+bool
+HCSR04::start_measurement_periodic(std::chrono::milliseconds period, const Callback<void(bool, float)> &cb) {
+
+    // return if a periodic measurement is already started, or if there are pending non-periodic measurements; move forward otherwise
+    // otherwise, post a periodic event to the queue, where the distance is measured and the callback called
+
+    if (is_periodic_started() || get_pending_measurement_count() > 0) {
+        return false;
+    }
+
+    auto id = queue.call_every(period, [this, cb] {
+
+        // start a pulse and sleep on the lock while the pulse does not return
+        // the lock is released in HCSR04::pulse_end_handler() when the pulse is completely received
+        // if the pulse takes too long (faulty sensor or object too far away), then wake-up anyways
+
+        start_pulse();
+
+        pulseBusyLock.try_acquire_for(SENSOR_TIMEOUT)
+        ? cb(true, dist)
+        : cb(false, 0.0f);
+    });
+
+    periodicId = id;
+    return periodicId != 0;
+}
+
+void
+HCSR04::stop_measurement_periodic() {
+
+    // return if no periodic measurement was registered; move forward otherwise
+    // if the object was not initialized, directly cancel the event and move forward
+    // otherwise, break the dispatch without acquiring shouldTerminate to cancel the event in the other thread
+
+    // because periodic and non-periodic measurements are exclusive, this method can never break pending non-periodic measurements
+
+    if (!is_periodic_started()) {
+        return;
+    }
+
+    if (!is_initialized()) {
+
+        queue.cancel(periodicId);
+        periodicId = 0;
+
+        return;
+    }
+
+    queue.break_dispatch();
+}
+
+bool
+HCSR04::is_periodic_started() const {
+
+    // if a periodic event was started, then it will have non-zero ID in the queue
+
+    return (periodicId != 0);
+}
+
+// Private methods
+
+void
+HCSR04::pulse_start_handler() {
+
+    // start the high-resolution timer
+
+    pulseTimer.start();
+}
+
+void
+HCSR04::pulse_end_handler() {
+
+    // stop the high-resolution timer, get its measured value and calculate the distance using the formula
+    // finally, reset the timer for the next use and release the pulseBusyLock to indicate that the pulse has been entirely received and processed
+
+    uint32_t pulse;
+
+    pulseTimer.stop();
+    pulse   = chrono::duration_cast<chrono::microseconds>(pulseTimer.elapsed_time()).count();
+    dist    = ((float)pulse * 343) / (10'000 * 2);
+
+    pulseTimer.reset();
+    pulseBusyLock.release();
+}
+
+__attribute__((always_inline))
+void
+HCSR04::start_pulse() {
+
+    // send a short 10ms pulse on the sensor
+
+    ThisThread::sleep_for(2ms);
+    trigPin = 1;
+    ThisThread::sleep_for(10ms);
+    trigPin = 0;
+}
+
+__attribute__((always_inline))
+void
+HCSR04::inc_pending_measurements() {
+    ++pendingMeasurementCount;
+}
+
+__attribute__((always_inline))
+void
+HCSR04::dec_pending_measurements() {
+    --pendingMeasurementCount;
+}
+
+
+void
+HCSR04::dispatch_events() {
+
+    // keep dispatching the queue in an infinite loop
+    //
+    // if break_dispatch is called without acquiring shouldTerminate,
+    // the periodic event is cancelled (if registered) and the queue is dispatched again
+    //
+    // if break_dispatch is called after acquiring shouldTerminate,
+    // the periodic event is cancelled (if registered) and the thread is prepared for graceful termination
+
+    for (;;) {
+
+        queue.dispatch_forever();
+
+        if (periodicId != 0) {
+
+            queue.cancel(periodicId);
+            periodicId = 0;
+        }
+
+        if (!shouldTerminate.try_acquire()) {
+            break;
+        }
+        shouldTerminate.release();
+    }
+
+    threadRunLock.release();
+}
---
+++ HCSR04.h
@@ -0,0 +1,209 @@
+/**
+ * @file                    HCSR04.h
+ * @author                  Aditya Agarwal (aditya.agarwal@dumblebots.com)
+ *
+ * @brief                   Simple Library to use an HCSR04 ultrasonic sensor with MBed OS asynchronously
+ *
+ * @copyright               Copyright (c) 2023
+ *
+ */
+
+#ifndef __HCSR04_H__
+#define __HCSR04_H__
+
+#include <utility>
+
+#include "mbed.h"
+
+/**
+ * @brief                   Class that provides a simple interface to use an HCSR04 ultrasonic sensor asynchronously
+ *
+ */
+class HCSR04 {
+
+    /** Trigger Pin of the sensor */
+    DigitalOut      trigPin;
+    /** Echo Pin of the sensor */
+    InterruptIn     echoPin;
+
+    /** Microsecond timer to measure the duration of a pulse */
+    Timer           pulseTimer;
+    /** Distance calculated from the duration of the pulse */
+    float           dist {0};
+
+    /** Handle to thread used for periodically reading from the sensor */
+    Thread          *threadHandle {nullptr};
+
+    /** Queue to post measurement event on */
+    EventQueue      queue;
+    /** ID of the periodic event on the EventQueue (0 if no periodic event or failed allocation) */
+    int32_t         periodicId {0};
+    /** Number of non-periodic measurements pending in the queue */
+    uint32_t        pendingMeasurementCount {0};
+
+    /** Semaphore to indicate whether a complete pulse has been received or not */
+    Semaphore       pulseBusyLock;
+    /** Semaphore to block the queue dispatch thread and for graceful termination */
+    Semaphore       shouldTerminate;
+
+    Semaphore       threadRunLock;
+
+public:
+
+    HCSR04() = delete;
+
+    /**
+     * @brief               Construct a new HCSR04 object
+     *
+     * @param   trig        Microcontroller Pin to which the Trig Pin of the sensor is connected
+     * @param   echo        Microcontroller Pin to which the Echo Pin of the sensor is connected
+     */
+    HCSR04(PinName trig, PinName echo);
+
+    /**
+     * @brief               Initializes the object by allocating and starting a thread to dispatch callbacks on
+     *
+     * @remark              If the thread was already initialized, then the HCSR04::finalize() method must be called
+     *                      before trying to initialize it again
+     *
+     * @attention           Can not call this method from ISR context
+     * @attention           It is unsafe to call this method from multiple threads concurrently
+     *
+     * @return              true if the thread was successfully allocated and started, false otherwise
+     */
+    bool        initialize();
+
+    /**
+     * @brief               Finalizes the object by stopping and freeing the thread on which callbacks are dispatched
+     *
+     * @remark              If the thread was not initialized or finalized before, then the HCSR04::initialize() method must be called
+     *                      before trying to finalize it again
+     * @remark              The object can not be finalized as long as there are pending non-periodic measurements
+     * @remark              The object can not be finalized as long as there is a registered non-periodic measurement
+     *
+     * @attention           Can not call this method from ISR context
+     * @attention           It is unsafe to call this method from multiple threads concurrently
+     *
+     * @return              true if the thread was successfully stopped and freed, false otherwise
+     */
+    bool        finalize();
+
+    /**
+     * @brief               Checks if the object was initialized and callbacks can be dispatched correctly
+     *
+     * @attention           This function can be called from ISR context
+     *
+     * @return              true if the thread is running, false otherwise
+     */
+    bool        is_initialized() const;
+
+    /**
+     * @brief               Asynchronously starts a measurement from the sensor and returns immediately, calling the callback once the measurement is complete
+     *
+     * @remarks             As long as periodic measurement is started, this function will always return false
+     * @remarks             As long as a measurement (enqueued using this method) is still pending, periodic measurement can not be started
+     *
+     * @attention           This function can be called from ISR context
+     *
+     * @param cb            Callback when the distance is calculated
+     *                      , the first argument to the callback is a boolean value which is false if the sensor timed-out, true otherwise
+     *                      , the second argument to the callback is the distance as floating point value
+     *
+     * @return              true if the request to start a measurement could successfully be enqueued, false otherwise
+     */
+    bool        do_measurement(const Callback<void(bool, float)> &cb);
+
+    /**
+     *
+     * @return
+     */
+    uint32_t    get_pending_measurement_count() const;
+
+    /**
+     * @brief               Begins asynchronously, periodically measuring the distance forever
+     *
+     * @remarks             To stop periodic measurement, see the HCSR04::stop_measurement_periodic() function
+     *
+     * @attention           This function can be called from ISR context
+     *
+     * @param period        Time period between two measurements
+     * @param cb            Callback when the distance is calculated
+     *                      , the first argument to the callback is a boolean value which is false if the sensor timed-out, true otherwise
+     *                      , the second argument to the callback is the distance as floating point value
+     *
+     * @return              true if the request to start a measurement could successfully be enqueued, false otherwise
+     */
+    bool        start_measurement_periodic(std::chrono::milliseconds period, const Callback<void(bool, float)> &cb);
+
+    /**
+     * @brief               Stops periodically measuring the distance
+     *
+     * @attention           Can not call from ISR context
+     *
+     * @remarks             If a measurement was happening while this function was called, the measurement (along with the callback) are completed first
+     *                      , rather than instantly stopping the measurements
+     *
+     * @attention           This function can be called from ISR context
+     */
+    void        stop_measurement_periodic();
+
+    /**
+     * @brief           Checks if periodic measurement was started
+     *
+     * @attention       This function can be called from ISR context
+     *
+     * @return          true if periodic measurement is started, false otherwise
+     */
+    bool        is_periodic_started() const;
+
+private:
+
+    /**
+     * @brief           Handler for the start of the returned pulse from the sensor
+     *
+     * @remarks         This is called whenever a rise interrupt is received on the Echo pin (start of a pulse)
+     */
+    void        pulse_start_handler();
+
+    /**
+     * @brief           Handler for the end of the returned pulse from the sensor
+     *
+     * @remarks         This is called whenever a fall interrupt is received on the Echo pin (end of a pulse)
+     */
+    void        pulse_end_handler();
+
+    /**
+     * @brief           Helper function to send a pulse to the sensor's Trig pin
+     */
+    __attribute__((always_inline))
+    void        start_pulse();
+
+    /**
+     * @brief           Helper function to atomically increment the count of pending measurements
+     *
+     * @todo            Make this function atomic
+     */
+    __attribute__((always_inline))
+    void        inc_pending_measurements();
+
+    /**
+     * @brief           Helper function to atomically decrement the count of pending measurements
+     *
+     * @todo            Make this function atomic
+     */
+    __attribute__((always_inline))
+    void        dec_pending_measurements();
+
+    /**
+     * @brief           Function for queue to run callbacks on
+     *
+     * @remarks         Calling break_dispatch() on the queue without acquiring shouldTerminate will cancel
+     *                  the registered periodic event (if at all) and dispatch the queue again
+     *
+     * @remarks         Calling break_dispatch() on the queue after acquiring shouldTerminate will cancel
+     *                  the registered periodic event (if at all) and prepare the thread for graceful termination
+     */
+    void        dispatch_events();
+};
+
+#endif //__HCSR04_H__
